edf947cf50f4e48b2f54e72c356ab31f57e8193c
aadityapatil12 <aadityapatil83089@gmail.com>
2022-09-21 13:05:43 +0530

    reviewModel

---
+++ src/models/reviewModel.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+const ObjectId = mongoose.Schema.Types.ObjectId
+
+const reviewSchema = new mongoose.Schema(
+    {
+        bookId: {type:ObjectId, require:true, ref:'Books'},
+        reviewedBy: {type:String, require:true, default: 'Guest'},
+        reviewedAt: {type:Date, require:true},
+        rating: {type:Number,enum:[1,2,3,4,5] ,require:true},
+        review: {type:String},
+        isDeleted: {type:Boolean, default: false},
+      },
+{ timestamps: true })
+module.exports = mongoose.model('Reviews', reviewSchema)
