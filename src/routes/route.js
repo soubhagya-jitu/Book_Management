18423a53b5f30e4fe99836599fa090e5b1149af9
AnujTembhurne <you@example.com>
2022-09-22 15:00:12 +0530

    Authentication

--- src/routes/route.js
+++ src/routes/route.js
@@ -3,14 +3,15 @@ const router = express.Router();
 
 const userController=require("../controllers/userController")
 const bookController=require("../controllers/bookController")
+const commonMw = require("../middlewares/commonMiddleware")
 
 
 router.post("/register",userController.createUser);
 router.post("/login", userController.userLogin)
-router.post("/books",bookController.createbook)
-router.get("/books",bookController.getBooks)
-router.get("/books/:bookId",bookController.getBooksDetail)
-router.put("/books/:bookId",bookController.putBooks)
-router.delete("/books/:bookId",bookController.deleteBookParam)
+router.post("/books",commonMw.authentication,bookController.createbook)
+router.get("/books",commonMw.authentication,bookController.getBooks)
+router.get("/books/:bookId",commonMw.authentication,bookController.getBooksDetail)
+router.put("/books/:bookId",commonMw.authentication,bookController.putBooks)
+router.delete("/books/:bookId",commonMw.authentication,bookController.deleteBookParam)
 
 module.exports = router;
